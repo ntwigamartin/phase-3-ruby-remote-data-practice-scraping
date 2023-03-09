19ca9bf42f817266d04b14a184298259c1f6494b
ntwigamartin <ntwiga.martin@gmail.com>
2023-03-09 23:56:26 +0300

    done

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,20 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
-html = open("https://flatironschool.com/")
+puts "Welcome to"
+
+
+html = URI.open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+newdoc = doc.css(".list-item")
+
+newdoc.each do|d|
+    puts d.text.strip
+end
+
+
+# binding.pry
+# pp doc
